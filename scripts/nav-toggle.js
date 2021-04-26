350a971ee9adf31ed6f458bc4159ea0942f70d3a
Meg Parsons <meg.l.parsons@gmail.com>
2021-04-26 09:14:18 -0400

    accessibility updates

--- scripts/nav-toggle.js
+++ scripts/nav-toggle.js
@@ -1,12 +1,8 @@
 const toggleBtn = document.getElementsByClassName("toggle-menu-btn")[0];
 const navbarLinks = document.getElementsByClassName("nav-links")[0];
-const barTop = document.getElementsByClassName('bar-top')[0];
-const barBottom = document.getElementsByClassName('bar-bottom')[0];
-const barMiddle = document.getElementsByClassName('bar-middle')[0]
 
 toggleBtn.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
-    barTop.classList.toggle('active');
-    barBottom.classList.toggle('active');
-    barMiddle.classList.toggle('active');
+    let expanded = toggleBtn.getAttribute('aria-expanded') === 'true' || false;
+    toggleBtn.setAttribute('aria-expanded', !expanded);
 });
